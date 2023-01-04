7725d05821050aaaed9e96635fd1f9e67872ad6b
Davi Ximenes <davixmnsl@gmail.com>
2023-01-03 21:22:54 -0300

    jsonignore

--- src/main/java/com/davixmns/projetospring/entities/Order.java
+++ src/main/java/com/davixmns/projetospring/entities/Order.java
@@ -14,8 +14,10 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant instant;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
