b958689a26f40cd71bb367482ae9853be7e0644f
Davi Ximenes <davixmnsl@gmail.com>
2023-01-05 21:42:15 -0300

    many to many

--- src/main/java/com/davixmns/projetospring/entities/Category.java
+++ src/main/java/com/davixmns/projetospring/entities/Category.java
@@ -1,5 +1,6 @@
 package com.davixmns.projetospring.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -16,6 +17,7 @@ public class Category implements Serializable {
     private String name;
 
     @ManyToMany(mappedBy = "categories")
+    @JsonIgnore
     private Set<Product> products = new HashSet<>();
 
     public Category(Long id, String name) {
